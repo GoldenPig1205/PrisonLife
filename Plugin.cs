77d1c5e1e8e8fbc5c1cd2d585af2ce3a0de808d3
GoldenPig1205 <78902671+GoldenPig1205@users.noreply.github.com>
2024-11-06 18:21:30 +0900

    프리즌 라이프ㅣ초안 작성

--- Plugin.cs
+++ Plugin.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Exiled.API.Features;
 
 namespace PrisonLife
 {
-    public class PrisonLife
+    public class PrisonLife : Plugin<Config>
     {
+        public static PrisonLife Instance;
+
+        public override string Name => base.Name;
+        public override string Author => "GoldenPig1205";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(12, 0, 5);
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            base.OnDisabled();
+        }
     }
 }
